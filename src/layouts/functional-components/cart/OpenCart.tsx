77d0b2f4ce36a51e6ad70dc4cd02f19c32d27d83
STEVEN ALEXANDER ALPIZAR GAMBOA <s.alpizar207@gmail.com>
2025-01-14 19:14:11 -0600

    fix: set default quantity to 0 and update quantity check in OpenCart component

--- src/layouts/functional-components/cart/OpenCart.tsx
+++ src/layouts/functional-components/cart/OpenCart.tsx
@@ -6,12 +6,12 @@ interface OpenCartProps {
   quantity?: number;
 }
 
-const OpenCart: React.FC<OpenCartProps> = ({ className = "", quantity }) => {
+const OpenCart: React.FC<OpenCartProps> = ({ className = "", quantity = 0 }) => {
   return (
     <div className="relative text-xl text-dark hover:text-primary dark:border-darkmode-border dark:text-white">
       <BsCart3 className={`dark:hover:text-darkmode-primary ${className}`} />
 
-      {quantity ? (
+      {quantity > 0 ? (
         <div className="bg-black text-white dark:bg-white dark:text-black text-xs rounded-full p-1 absolute -top-1 md:-top-2 -right-3 md:-right-4 w-5 h-5 flex items-center justify-center">
           {quantity}
         </div>
